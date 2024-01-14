c6216d5005faed7e37d8e06dfee4edc5fbdcaa80
DenkingOfficial <shershnev.andrei@gmail.com>
2024-01-15 01:49:59 +0500

    Last fix

--- airflow/dags/dag_text_class.py
+++ airflow/dags/dag_text_class.py
@@ -4,7 +4,7 @@ import datetime as dt
 
 args = {
     "owner": "admin",
-    "start_date": dt.datetime(2024, 1, 14, 23, 50),
+    "start_date": dt.datetime(2024, 1, 14, 20, 40),
     "retries": 1,
     "retry_delays": dt.timedelta(minutes=1),
     "depends_on_past": False,
@@ -19,22 +19,22 @@ with DAG(
 ) as dag:
     data_download = BashOperator(
         task_id="data_download",
-        bash_command="python /home/mlserv/mlops-final/scripts/data_download.py",
+        bash_command="python3 /home/mlserv/mlops-final/scripts/data_download.py",
         dag=dag,
     )
     data_preprocess = BashOperator(
         task_id="data_preprocess",
-        bash_command="python /home/mlserv/mlops-final/scripts/data_preprocess.py",
+        bash_command="python3 /home/mlserv/mlops-final/scripts/data_preprocess.py",
         dag=dag,
     )
     model_train = BashOperator(
         task_id="model_train",
-        bash_command="python /home/mlserv/mlops-final/scripts/model_train.py",
+        bash_command="python3 /home/mlserv/mlops-final/scripts/model_train.py",
         dag=dag,
     )
     model_test = BashOperator(
         task_id="model_test",
-        bash_command="python /home/mlserv/mlops-final/scripts/model_test.py",
+        bash_command="python3 /home/mlserv/mlops-final/scripts/model_test.py",
         dag=dag,
     )
     data_download >> data_preprocess >> model_train >> model_test
